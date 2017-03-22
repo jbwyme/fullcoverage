9333e62fb51dac296d562e6f240070015ddb552d
Josh Wymer <wymetyme@gmail.com>
2017-03-21 21:24:07 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+/* global require */
+'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+var server = app.listen(process.env.PORT || 3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
