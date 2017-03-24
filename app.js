2eb2085f07e519e9b0f254f375dca0c0a5e64c64
Josh Wymer <wymetyme@gmail.com>
2017-03-23 23:30:37 -0500

    basic working chrome extension

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+app.use(express.static(`./build`));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
